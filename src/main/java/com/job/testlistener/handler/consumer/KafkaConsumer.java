f819b13731710455b7b7b0e5c911fcd13131c6fb
EnzeoX <enzeox@gmail.com>
2024-01-17 01:13:32 +0200

    Changed method name

--- src/main/java/com/job/testlistener/handler/consumer/KafkaConsumer.java
+++ src/main/java/com/job/testlistener/handler/consumer/KafkaConsumer.java
@@ -1,8 +1,6 @@
 package com.job.testlistener.handler.consumer;
 
 import com.job.testlistener.service.HapiFhriService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +8,6 @@ import java.util.Objects;
 
 @Component
 public class KafkaConsumer {
-
-    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
 
     private final HapiFhriService hapiFhriService;
 
@@ -24,6 +20,6 @@ public class KafkaConsumer {
             groupId = "${spring.kafka.consumer.group-id}")
     public void consumeMessage(String message) {
         Objects.requireNonNull(message);
-        hapiFhriService.sendBundleToHapi(message);
+        hapiFhriService.sendEncounterBundleToHapi(message);
     }
 }
