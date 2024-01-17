4b953d0c96d12397fddc0c9e3ca7cfb4c8a32331
EnzeoX <enzeox@gmail.com>
2024-01-17 18:36:41 +0200

    Added logging to KafkaConsumer

--- src/main/java/com/job/testlistener/handler/consumer/KafkaConsumer.java
+++ src/main/java/com/job/testlistener/handler/consumer/KafkaConsumer.java
@@ -1,6 +1,8 @@
 package com.job.testlistener.handler.consumer;
 
 import com.job.testlistener.service.HapiFhriService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +13,8 @@ public class KafkaConsumer {
 
     private final HapiFhriService hapiFhriService;
 
+    private final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
     public KafkaConsumer(HapiFhriService hapiFhriService) {
         this.hapiFhriService = hapiFhriService;
     }
@@ -20,6 +24,7 @@ public class KafkaConsumer {
             groupId = "${spring.kafka.consumer.group-id}")
     public void consumeMessage(String message) {
         Objects.requireNonNull(message);
+        log.info("Kafka message collected:\n {}", message);
         hapiFhriService.sendEncounterBundleToHapi(message);
     }
 }
